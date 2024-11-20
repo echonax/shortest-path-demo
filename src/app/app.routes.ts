caa0fd3e4e3d5ab4e76bb5961ca638c862bf96e4
eko <canekincam@gmail.com>
2024-11-20 21:40:47 +0100

    only dist

--- src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { TopologyComponent } from './topology.component';
-
-export const routes: Routes = [
-  {title: "Topology", path:'topology', component: TopologyComponent}
-];
